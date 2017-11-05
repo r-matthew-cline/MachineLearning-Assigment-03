338caf9d7891cc05a5041383fd85b66218b3afa9
Matthew Cline <cline.48@wright.edu>
2017-11-05 01:19:11 -0400

    Added main file with boiler

---
+++ main.py
@@ -0,0 +1,12 @@
+###############################################################################
+##
+## Assignment 3: Neural Networks
+##
+## @author: Matthew Cline
+## @version: 20171104
+##
+## Description: Neural network program to use on the Flu Data from class and
+## the WINE dataset.
+##
+###############################################################################
+
